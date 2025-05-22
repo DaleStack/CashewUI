b9978a4a2d18ef13bd08174909823a962568589e
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-05-22 21:12:21 +0800

    made button page mobile responsive

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ct(r0na77myeu2sbiwdjw3gs94f9*k3+svz7uvw8=&o3!^^bb7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.50.179']
 
 
 # Application definition
